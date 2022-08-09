72052abc359d01d6447947c10f86684a3edd5569
Fernando Jojoa <luis.jojoa.q@gmail.com>
2022-08-09 13:27:18 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -2,51 +2,49 @@ var storage = window.sessionStorage;
 let input = null;
 
 var headers = new Headers();
-    headers.append("X-RapidAPI-Host", "deezerdevs-deezer.p.rapidapi.com");
-    headers.append("X-RapidAPI-Key", "5a5cf0081dmsh49d2c653a5f76d5p19598djsndaeceaacd74c");
+headers.append("X-RapidAPI-Host", "deezerdevs-deezer.p.rapidapi.com");
+headers.append(
+  "X-RapidAPI-Key",
+  "5a5cf0081dmsh49d2c653a5f76d5p19598djsndaeceaacd74c"
+);
 
 var opciones = {
-        method: 'GET',
-        headers: headers,
-        mode: 'cors',
-        cache: 'default'
+  method: "GET",
+  headers: headers,
+  mode: "cors",
+  cache: "default",
 };
 
 function start() {
-    
-    input = document.querySelector("input");
-    input.onkeyup = (e) => {
-        if( e.key == "Enter"){
-            artist = input.value;
-            query();
-        }
+  input = document.querySelector("input");
+  input.onkeyup = (e) => {
+    if (e.key == "Enter") {
+      artist = input.value;
+      query();
     }
+  };
 }
 
-    //Fetch al API
-    
-function query(){
+//Fetch al API
 
-    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artist}`, opciones)
-        .then((response) => {
-            return response.json()
-        })
-        .then((json) => {
-            const data = json.data;
+function query() {
+  fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artist}`, opciones)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      const data = json.data;
 
-            if (data.length > 0) {
-                storage.setItem("songs",JSON.stringify(data));
-                redirect(); 
-            } else {
-                console.log("No existe ese artista");
-                input.value = "";
-                
-            }
-            
-        })
-    }
-    
-        function redirect(){
-            window.location = "./player.html";
-        }
-    
+      if (data.length > 0) {
+        storage.setItem("songs", JSON.stringify(data));
+        redirect();
+      } else {
+        console.log("No existe ese artista");
+        input.value = "";
+      }
+    });
+}
+
+function redirect() {
+  window.location = "./player.html";
+}
